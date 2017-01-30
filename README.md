d843bc11c408bc063452ad1b6134311f548c9494
ChangJoo Park <changjoo.park@partner.so>
2017-01-30 16:38:22 +0900

    Initialize Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Electron boilerplate :)
+
+
+## Installation
+
+```terminal
+git clone https://github.com/ChangJoo-Park/electron-boilerplate my-electron-app
+cd my-electron-app
+npm install
+npm run postinstall
+```
+
+## Development
+
+```terminal
+npm run dev
+```
+
+## Packaging
+
+```terminal
+npm run dist
+```
---
+++ app/main.js
@@ -0,0 +1,26 @@
+'use strict'
+const electron = require('electron')
+const updater = require('electron-updater')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+
+}
+
+app.on('ready', createWindow)
