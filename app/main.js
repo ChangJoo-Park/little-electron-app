a2e78ceb62a8929662661203b75f058603702f05
ChangJoo Park <pcjpcj2@gmail.com>
2017-01-30 17:17:17 +0900

    working on electron-updater

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 'use strict'
 const electron = require('electron')
-const updater = require('electron-updater')
+const autoUpdater = require('electron-updater')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
@@ -18,9 +18,25 @@ function createWindow () {
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
-  }))
+  }))  
+  // autoUpdater.signals.updateDownloaded((info) => {
+  //   notify("A new update is ready to install", `Version ${info.version} is downloaded and will be automatically installed on Quit`)
+  // })
+}
+function notify(title, message) {
+  let windows = BrowserWindow.getAllWindows()
+  console.log(windows)
+  if (windows.length == 0) {
+    return
+  }
 
-
+  windows[0].webContents.send("notify", title, message)
 }
 
 app.on('ready', createWindow)
+
+
+const log = require('electron-log')
+log.transports.file.leve = 'info'
+autoUpdater.logger = log
+console.log(autoUpdater.app)
